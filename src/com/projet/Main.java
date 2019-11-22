a6e11203afba0939aa9cc9de4ac38a31278049b7
yohann <yohann.valo@utt.fr>
2019-11-22 15:37:39 +0100

    Score added

--- src/com/projet/Main.java
+++ src/com/projet/Main.java
@@ -3,13 +3,7 @@ package com.projet;
 public class Main {
 
     public static void main(String[] args) {
-        //Game.getInstance();
-        Player player = new Player("TARACE");
-        player.addCardFaceDown(new Card(Color.Spade,3), new Card(Color.Spade,2));
-
-
-
-
+        Game.getInstance();
     }
 
 }
--- src/com/projet/Score.java
+++ src/com/projet/Score.java
@@ -7,7 +7,12 @@ public class Score {
         points = 0;
         this.player = player;
     }
-
+    public void calculateAll(){
+        calculateSpade();
+        calculateClub();
+        calculateDiamond();
+        calculateHeart();
+    }
     public void calculateSpade() {
         for (Card card : player.getJest()) {
             if (card.getColor() == Color.Spade) {
@@ -15,6 +20,53 @@ public class Score {
                 this.points += card.getValue();
             }
         }
+    }
+    public void calculateClub() {
+        for (Card card : player.getJest()) {
+            if (card.getColor() == Color.Club) {
+
+                this.points += card.getValue();
+            }
+        }
+    }
+    public void calculateDiamond() {
+        for (Card card : player.getJest()) {
+            if (card.getColor() == Color.Diamond) {
+
+                this.points -= card.getValue();
+            }
+        }
+    }
+    public void calculateHeart() {
+        boolean hasJoker = false;
+        int nbrHeart = 0;
+        for (Card card : player.getJest()) {
+            if (card.getColor() == Color.Jocker) {
+                hasJoker = true;
+            }
+            if (card.getColor() == Color.Heart) {
+                nbrHeart += 1;
+            }
+
+        }
+        if(hasJoker== true && nbrHeart == 0){
+            this.points += 4;
+        }
+        else if(hasJoker == true && nbrHeart <4 && nbrHeart >0) {
+            for (Card card : player.getJest()) {
+                if (card.getColor() == Color.Heart) {
+                    this.points -= card.getValue();
+                }
+            }
+        }
+        else if(hasJoker == true && nbrHeart ==4) {
+            for (Card card : player.getJest()) {
+                if (card.getColor() == Color.Heart) {
+                    this.points += card.getValue();
+                }
+            }
+        }
+
     }
 
     public int getPoints() {
