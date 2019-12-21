9b0fe5e9c4a3c472b67792f303a92548892dd72b
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-12-21 01:09:15 +0100

    Menu working

--- src/com/projet/views/Interface.java
+++ src/com/projet/views/Interface.java
@@ -16,26 +16,12 @@ public class Interface implements Observer {
 
     private JFrame frame;
 
-    private JLabel title;
-
-    private JButton button0;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-
     /**
      * Create the application.
      */
     public Interface() {
         initialize();
         Game.getInstance().addObserver(this);
-
-        new ButtonController(button0, 0);
-        new ButtonController(button1, 1);
-        new ButtonController(button2, 2);
-        new ButtonController(button3, 3);
-        new ButtonController(button4, 4);
     }
 
     /**
@@ -46,31 +32,10 @@ public class Interface implements Observer {
         frame.setResizable(false);
         frame.setBounds(100, 100, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setLayout(null);
 
-        title = new JLabel("Combien de joueurs ?");
-        title.setBounds(125, 61, 160, 14);
-        frame.getContentPane().add(title);
-
-        button0 = new JButton("0");
-        button0.setBounds(77, 87, 89, 23);
-        frame.getContentPane().add(button0);
-
-        button1 = new JButton("1");
-        button1.setBounds(196, 86, 89, 23);
-        frame.getContentPane().add(button1);
-
-        button2 = new JButton("2");
-        button2.setBounds(77, 121, 89, 23);
-        frame.getContentPane().add(button2);
-
-        button3 = new JButton("3");
-        button3.setBounds(196, 120, 89, 23);
-        frame.getContentPane().add(button3);
-
-        button4 = new JButton("4");
-        button4.setBounds(135, 161, 89, 23);
-        frame.getContentPane().add(button4);
+        Menu menu = new Menu();
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(menu);
     }
 
     public JFrame getFrame() {
@@ -79,10 +44,6 @@ public class Interface implements Observer {
 
     @Override
     public void update(EventType eventType, Object payload) {
-        switch (eventType) {
-            case CHOOSE_PLAYER_COUNT:
-            case CHOOSE_BOT_COUNT:
-                title.setText((String) payload);
-        }
+
     }
 }
---
+++ src/com/projet/views/Menu.java
@@ -0,0 +1,187 @@
+/*
+ * Created by JFormDesigner on Fri Dec 20 23:07:15 CET 2019
+ */
+
+package com.projet.views;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.concurrent.BlockingQueue;
+import javax.swing.*;
+import javax.swing.event.*;
+
+import com.projet.models.App;
+import net.miginfocom.swing.*;
+
+/**
+ * @author unknown
+ */
+public class Menu extends JPanel {
+
+    private boolean easyDifficulty = true;
+
+    /*
+    0: pas d'extension
+    1: remise en jeu des cartes dans le stack
+    2: jouer carte face caché
+     */
+    private int extension = 0;
+
+    public Menu() {
+        initComponents();
+    }
+
+    private void playActionPerformed(ActionEvent event) {
+        try {
+            BlockingQueue<String> queue = App.getInstance().getQueue();
+
+            queue.put(String.valueOf(playerInput.getModel().getValue()));
+            queue.put(String.valueOf(botInput.getModel().getValue()));
+            queue.put(String.valueOf(easyDifficulty ? 1 : 2));
+
+            // Voulez vous jouer à une extension
+            queue.put(String.valueOf(extension == 0 ? 2 : 1));
+
+            if(extension != 0) {
+                queue.put(String.valueOf(extension));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playerStateChanged(ChangeEvent e) {
+        int players = (int) playerInput.getModel().getValue();
+        System.out.println(players);
+
+        SpinnerModel botModel = new SpinnerNumberModel(0, 0, players, 1);
+        botInput.setModel(botModel);
+    }
+
+    private void difficultyActionPerformed(ActionEvent e) {
+        easyDifficulty = !easyDifficulty;
+
+        difficulty.setText(easyDifficulty ? "Facile" : "Difficile");
+    }
+
+    private void extensionActionPerformed(ActionEvent e) {
+        extension++;
+        extension %= 3;
+        String text = null;
+        switch (extension) {
+            case 0:
+                text = "Pas d'extension";
+                break;
+
+            case 1:
+                text = "Remise en jeu";
+                break;
+
+            case 2:
+                text = "Face caché";
+                break;
+        }
+
+        extensionButton.setText(text);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+        title = new JLabel();
+        players = new JLabel();
+        playerInput = new JSpinner();
+        bots = new JLabel();
+        botInput = new JSpinner();
+        difficultyLabel = new JLabel();
+        difficulty = new JButton();
+        extensionLabel = new JLabel();
+        extensionButton = new JButton();
+        play = new JButton();
+
+        //======== this ========
+        setBorder (new javax. swing. border. CompoundBorder( new javax .swing .border .TitledBorder (new javax. swing.
+        border. EmptyBorder( 0, 0, 0, 0) , "JF\u006frmDes\u0069gner \u0045valua\u0074ion", javax. swing. border. TitledBorder. CENTER
+        , javax. swing. border. TitledBorder. BOTTOM, new java .awt .Font ("D\u0069alog" ,java .awt .Font
+        .BOLD ,12 ), java. awt. Color. red) , getBorder( )) );  addPropertyChangeListener (
+        new java. beans. PropertyChangeListener( ){ @Override public void propertyChange (java .beans .PropertyChangeEvent e) {if ("\u0062order"
+        .equals (e .getPropertyName () )) throw new RuntimeException( ); }} );
+        setLayout(new MigLayout(
+            "fill,hidemode 3,align center center",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]" +
+            "[]"));
+
+        //---- title ----
+        title.setText("Jest");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+        add(title, "cell 0 0 2 1");
+
+        //---- players ----
+        players.setText("Nombre de joueurs");
+        players.setHorizontalAlignment(SwingConstants.CENTER);
+        add(players, "cell 0 1");
+
+        //---- playerInput ----
+        playerInput.setModel(new SpinnerNumberModel(3, 3, 4, 1));
+        playerInput.addChangeListener(e -> playerStateChanged(e));
+        add(playerInput, "cell 1 1");
+
+        //---- bots ----
+        bots.setText("Nombre de bots");
+        bots.setHorizontalAlignment(SwingConstants.CENTER);
+        add(bots, "cell 0 2");
+
+        //---- botInput ----
+        botInput.setModel(new SpinnerNumberModel(0, 0, 4, 1));
+        add(botInput, "cell 1 2");
+
+        //---- difficultyLabel ----
+        difficultyLabel.setText("Difficult\u00e9");
+        difficultyLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(difficultyLabel, "cell 0 3");
+
+        //---- difficulty ----
+        difficulty.setText("Facile");
+        difficulty.addActionListener(e -> difficultyActionPerformed(e));
+        add(difficulty, "cell 1 3");
+
+        //---- extensionLabel ----
+        extensionLabel.setText("Extension");
+        extensionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        add(extensionLabel, "cell 0 4");
+
+        //---- extensionButton ----
+        extensionButton.setText("Pas d'extension");
+        extensionButton.addActionListener(e -> extensionActionPerformed(e));
+        add(extensionButton, "cell 1 4");
+
+        //---- play ----
+        play.setText("Jouer");
+        play.addActionListener(e -> playActionPerformed(e));
+        add(play, "cell 0 5 2 1");
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    private JLabel title;
+    private JLabel players;
+    private JSpinner playerInput;
+    private JLabel bots;
+    private JSpinner botInput;
+    private JLabel difficultyLabel;
+    private JButton difficulty;
+    private JLabel extensionLabel;
+    private JButton extensionButton;
+    private JButton play;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
