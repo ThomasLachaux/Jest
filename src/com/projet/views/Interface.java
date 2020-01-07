e53cab310f106bd89bdb127ecd91dfb44b20325c
yohann <yohann.valo@utt.fr>
2020-01-07 15:32:50 +0100

    taille

--- src/com/projet/views/Interface.java
+++ src/com/projet/views/Interface.java
@@ -45,7 +45,7 @@ public class Interface extends Observable implements Observer {
         }
         frame = new JFrame("Jest");
         frame.setResizable(false);
-        frame.setBounds(100, 100, 1080, 720);
+        frame.setBounds(100, 100, 1000, 1000);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         switchPanel(new Menu());
